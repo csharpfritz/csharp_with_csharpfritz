2a1108dabf03ac197c58446ce3f7e270d024b7b9
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-09-06 17:33:25 -0400

    Formatted code

--- src/NotebookUnit/RunTestsMagicCommand.cs
+++ src/NotebookUnit/RunTestsMagicCommand.cs
@@ -8,42 +8,43 @@ using Microsoft.DotNet.Interactive.Formatting;
 
 namespace NotebookUnit
 {
-  public class RunTestsMagicCommand : IKernelExtension
-  {
-    public async Task OnLoadAsync(Kernel kernel)
-    {
+	public class RunTestsMagicCommand : IKernelExtension
+	{
+		public async Task OnLoadAsync(Kernel kernel)
+		{
 
-      var testCommand = new Command("#!test", "Run the tests in this notebook")
-      {
-        new Option<Action[]>(new string[] {"-t", "--tests" }, () => new Action[] { }, "Specify tests to run")
-      };
+			var testCommand = new Command("#!test", "Run the tests in this notebook")
+			{
+				new Option<Action[]>(new string[] {"-t", "--tests" }, () => new Action[] { }, "Specify tests to run")
+			};
 
-      testCommand.Handler =
-      CommandHandler.Create(async (Action[] tests, KernelInvocationContext context) =>
-        RunTests(tests, context));
+			testCommand.Handler =
+			CommandHandler.Create(async (Action[] tests, KernelInvocationContext context) =>
+				RunTests(tests, context));
 
-      kernel.AddDirective(testCommand);
+			kernel.AddDirective(testCommand);
 
-      if (KernelInvocationContext.Current is { } context)
-      {
-        await context.DisplayAsync($"`{nameof(RunTestsMagicCommand)}` is loaded. It enables testing in notebooks", "text/markdown");
-      }
+			if (KernelInvocationContext.Current is { } context)
+			{
+				await context.DisplayAsync($"`{nameof(RunTestsMagicCommand)}` is loaded. It enables testing in notebooks", "text/markdown");
+			}
 
-    }
+		}
 
 		private object RunTests(Action[] tests, KernelInvocationContext context)
 		{
 
-      if (tests == null || tests.Length == 0) {
-        context.Fail(message: "No tests");
-        return null;
+			if (tests == null || tests.Length == 0)
+			{
+				context.Fail(message: "No tests");
+				return null;
 			}
 
-      context.Publish(new DisplayedValueProduced("Tests to be run", context.Command, new[] {
-        new FormattedValue(PlainTextFormatter.MimeType, "Tests will be run")
-      }));
+			context.Publish(new DisplayedValueProduced("Tests to be run", context.Command, new[] {
+				new FormattedValue(PlainTextFormatter.MimeType, "Tests will be run")
+			}));
 
-      return new object();
+			return new object();
 
 		}
 
