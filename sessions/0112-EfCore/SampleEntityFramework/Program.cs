1f29ef19d8685295e50cb530ecdf948c21723c97
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-11-22 21:54:45 -0500

    Initialized session 12 samples

---
+++ sessions/0112-EfCore/SampleEntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
