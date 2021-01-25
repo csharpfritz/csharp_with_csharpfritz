1fb1f16f5056e0ca3c249406549deb99bd528bac
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-01-24 23:04:50 -0500

    Added gRPC client

---
+++ sessions/Season-02/0205-ApiPartTwo/src/4_Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace _4_Client
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+
+			using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+			var client = new Greeter.GreeterClient(channel);
+			var reply = await client.SayHelloAsync(
+
+				new HelloRequest() { Name = "Fritz" });
+
+			Console.WriteLine($"Greeting: {reply.Message}");
+
+		}
+	}
+}
