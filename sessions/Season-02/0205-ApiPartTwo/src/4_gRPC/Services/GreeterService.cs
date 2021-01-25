b8c3a07ed8657b8ba3ac0dedfe9dccc8cfa648ac
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-01-25 07:43:09 -0500

    Updated docs and added streaming sample

--- sessions/Season-02/0205-ApiPartTwo/src/4_gRPC/Services/GreeterService.cs
+++ sessions/Season-02/0205-ApiPartTwo/src/4_gRPC/Services/GreeterService.cs
@@ -2,25 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
 namespace _4_gRPC
 {
-    public class GreeterService : Greeter.GreeterBase
+  public class GreeterService : Greeter.GreeterBase
+  {
+    private readonly ILogger<GreeterService> _logger;
+    public GreeterService(ILogger<GreeterService> logger)
     {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
-        {
-            _logger = logger;
+      _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+      return Task.FromResult(new HelloReply
+      {
+        Message = "Hello " + request.Name
+      });
+    }
+
+    public override async Task CountStream(Empty request, IServerStreamWriter<CountData> responseStream, ServerCallContext context)
+    {
+
+        for (var i=0; i<10; i++) {
+
+            if (context.CancellationToken.IsCancellationRequested) break;
+
+            await Task.Delay(500);
+
+            await responseStream.WriteAsync(new CountData {
+                Count=i,
+                Timestamp = Timestamp.FromDateTime(DateTime.Now)
+            });
+
         }
 
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(new HelloReply
-            {
-                Message = "Hello " + request.Name
-            });
-        }
     }
+
+
+
+  }
 }
