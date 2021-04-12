940f062b32f3ebe77e6b4cf2ada386dc07aaca9d
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-04-12 08:51:54 -0400

    Pre-Show

---
+++ sessions/Season-03/0305-Services/src/Services/SignalR/Client/wwwroot/js/emote.js
@@ -0,0 +1,25 @@
+﻿export function flyEmote(buttonId) {
+
+	var newImg = document.createElement("img");
+	newImg.src = `/img/${buttonId}.png`;
+	newImg.className = "float";
+	newImg.style.bottom = "-30px";
+	newImg.style.left = `${Math.random() * 100}%`;
+	document.body.appendChild(newImg);
+
+}
+
+export function start() {
+
+	window.setInterval(function () {
+
+		var top = document.body.clientHeight - 500 - 100;
+
+		var hiddenEls = document.querySelectorAll("img");
+		hiddenEls.forEach(h => {
+			if (top > h.offsetTop) document.body.removeChild(h);
+		});
+
+	}, 2000);
+
+}
