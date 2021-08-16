931e5f94c767586608a88059dedd93f2f295e674
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-08-16 08:57:22 -0400

    Episode 0504 (#83)
    * Added samples for stream
    
    * After session

---
+++ sessions/Season-05/0504-BuildYourFirstAnalyzer/src/TestProject/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			while (1 != 0)
+				if (true) 
+					Console.WriteLine("This is a test");
+
+				//Console.WriteLine("This will never run");
+
+		}
+	}
+}
