895c225b9f290e288ffd7b15f797731776099fb8
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-11-01 13:07:45 -0400

    Added Live folder from the Nov 1, 2021 stream

---
+++ sessions/Season-01/0110-UnitTests/Live/MyTests/GivenMyCalculator/WhenAddingTwoNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyTests.GivenMyCalculator
+{
+    public class WhenAddingTwoNumbers
+    {
+
+        [Fact]
+        public void ThenTheyShouldAddProperly()
+        {
+
+            // Arrange
+            var calc = new MyLibrary.Calculator();
+            var add1 = 2;
+            var add2 = 3;
+
+            // Act
+            var result = calc.Sum(add1, add2);
+
+            // Assert
+            Assert.Equal(5, result);
+
+
+        }
+
+    }
+}
