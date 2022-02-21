b6d434c05486cfcee1f4e62076afd2b0abe7ee24
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2022-02-21 07:57:10 -0500

    Updated SignalR demo to show connected clients and total clicks

--- sessions/Season-03/0305-Services/src/Services.OpenAPI/Client/Program.cs
+++ sessions/Season-03/0305-Services/src/Services.OpenAPI/Client/Program.cs
@@ -18,6 +18,7 @@ namespace Services.OpenAPI.Client
 			builder.RootComponents.Add<App>("#app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped(sp => new WeatherForecastClient("/", new HttpClient() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) } ));
 
 			await builder.Build().RunAsync();
 		}
