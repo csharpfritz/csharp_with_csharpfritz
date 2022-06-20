b509936853eeb208f356c2bfa6246a1e6d529f4d
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2022-06-20 07:34:06 -0400

    Cleaned up warnings

--- sessions/Season-02/CollectionWebsite/1204-ConfigurationAndData/src/Models/CollectionContext.cs
+++ sessions/Season-02/CollectionWebsite/1204-ConfigurationAndData/src/Models/CollectionContext.cs
@@ -33,6 +33,6 @@ public class CollectionContext : DbContext
         base.OnModelCreating(modelBuilder);
     }
 
-    public DbSet<CollectionItem> CollectionItems { get; set; }
+    public DbSet<CollectionItem> CollectionItems => Set<CollectionItem>();
 
 }
--- sessions/Season-02/CollectionWebsite/1204-ConfigurationAndData/src/Models/CollectionItem.cs
+++ sessions/Season-02/CollectionWebsite/1204-ConfigurationAndData/src/Models/CollectionItem.cs
@@ -9,7 +9,7 @@ public class CollectionItem
  public int Id { get; set; }
 
  [Required]
- public string Name { get; set; }
+ public string Name { get; set; } = null!;
 
  public string? Description { get; set; }
 
