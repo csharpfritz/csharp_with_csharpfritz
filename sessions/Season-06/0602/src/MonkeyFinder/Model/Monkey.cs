3a86b664b998eb2728faf51a82fff70a6caf4228
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-02-27 08:58:10 -0500

    After stream

--- sessions/Season-06/0602/src/MonkeyFinder/Model/Monkey.cs
+++ sessions/Season-06/0602/src/MonkeyFinder/Model/Monkey.cs
@@ -2,5 +2,11 @@
 
 public class Monkey
 {
-
+	public string Name { get; set; }
+	public string Location { get; set; }
+	public string Details { get; set; }
+	public string Image { get; set; }
+	public int Population { get; set; }
+	public float Latitude { get; set; }
+	public float Longitude { get; set; }
 }
